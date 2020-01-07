a1f1105cc44380dd3bd54f67226b4a4ee466e4f3
arkaanashadi <arkaan.ashadi@gmail.com>
2020-01-07 22:39:50 +0700

    fixed keyboardinterrupt typo

--- pi-code/main.py
+++ pi-code/main.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     while(True):
         try:
             main()
-        except(KeyboardInterupt):
+        except KeyboardInterrupt:
             GPIO.cleanup()
             break
 
